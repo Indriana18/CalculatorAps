69a21eeaf41a2da8a875704d9a9fa34cd44aa492
Indriana18 <48426491+Indriana18@users.noreply.github.com>
2019-09-27 05:56:15 +0700

    Add files via upload

---
+++ CalculatorApp/CalculatorConsole/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Aplikasi Calculator Versi Console";
+
+            Console.Write("Inputkan nilai a = ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Inputkan niali b = ");
+            var b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
+            Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}",a ,b, Pengurangan(a, b));
+            Console.WriteLine("Hasil Perkalian: {0} * {1} = {2}",a ,b , Perkalian(a, b));
+            Console.WriteLine("Hasil Pembagian: {0} / {1} = {2}",a ,b , Pembagian(a, b));
+            Console.ReadKey();
+        }
+        static int Penambahan(int a, int b)
+        {
+            return a + b;
+        }
+        static int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+        static int Perkalian(int a, int b)
+        {
+            return a * b;
+        }
+        static int Pembagian(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
